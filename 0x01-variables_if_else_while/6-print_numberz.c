c1d40406438902cb1c11466ec0c26c5aac7519d0
fatimazyani <sdzyani@gmail.com>
2023-02-17 12:53:19 -0800

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - prints the Numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int c = 0;
+
+	while (c <= 9)
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
