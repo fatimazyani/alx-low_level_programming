1c221c58dd7177fe6d887b3bf7b0c9372391c4e6
fatimazyani <123373140+fatimazyani@users.noreply.github.com>
2023-03-01 00:00:01 +0100

    task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,28 +1,18 @@
 #include "main.h"
 
 /**
- * largest_number - returns the largest of 3 numbers
- * @a: first integer
- * @b: second integer
- * @c: third integer
- * Return: largest number
- */
+ * _strlen - find the length of a string
+ * @s: pointer to the string to check
+ * Return: void
+*/
 
-int largest_number(int a, int b, int c)
-{int largest;
 
-if (a > b && a > c)
+int _strlen(char *s)
 {
-largest = a;
-}
-else if (b > c)
-{
-largest = b;
-}
-else
-{
-largest = c;
-}
+	int i = 0;
 
-return (largest);
+	while (s[i])
+		i++;
+
+	return (i);
 }
