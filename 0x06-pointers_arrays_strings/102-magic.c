93139892a8cf067f04fa9266f9fcb2f2457fa731
fatimazyani <123373140+fatimazyani@users.noreply.github.com>
2023-03-02 22:50:11 +0100

    TASK 102

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,31 +1,23 @@
-#include "main.h"
+#include <stdio.h>
 
-/**
- * _atoi - convert a string to an integer.
- * @s: char type string
- * Return: integer converted
- */
-
-int _atoi(char *s)
+int main(void)
 {
-	int i;
-	int res = 0;
-	int sig = -1;
-	int brk = 0;
+  int n;
+  int a[5];
+  int *p;
 
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		if (s[i] == '-')
-			sig = sig * -1;
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			res = res * 10;
-			res -= (s[i] - '0');
-			brk = 1;
-		}
-		else if (brk == 1)
-			break;
-	}
-	res = sig * res;
-	return (res);
+  a[2] = 1024;
+  p = &n;
+  /*
+   * write your line of code here...
+   * Remember:
+   * - you are not allowed to use a
+   * - you are not allowed to modify p
+   * - only one statement
+   * - you are not allowed to code anything else than this line of code
+   */
+ p[5] = 98;
+  /* ...so that this prints 98\n */
+  printf("a[2] = %d\n", a[2]);
+  return (0);
 }
