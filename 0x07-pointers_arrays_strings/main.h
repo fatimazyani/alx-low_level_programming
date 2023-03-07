73ce129928cbbc22b0a1e8c33ee1d57a45382693
fatimazyani <123373140+fatimazyani@users.noreply.github.com>
2023-03-07 23:33:57 +0100

    main

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,22 +1,15 @@
-#include "main.h"
-/**
- * *_strcat - concatenates two strings
- * @dest: pointer destination
- * @src: pointer source
- * Return: void
-*/
+#ifndef FILE_MAIN
+#define FILE_MAIN
 
-char *_strcat(char *dest, char *src)
-{
-int a = -1, i;
-for (i = 0; dest[i] != '\0'; i++)
-;
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
+int _putchar(char c);
+void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
 
-do {
-	a++;
-	dest[i] = src[a];
-	i++;
-} while (src[a] != '\0');
-
-return (dest);
-}
+#endif
