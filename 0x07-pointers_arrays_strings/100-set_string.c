07f35ab9614897b7e0b03dc02ef201cc404cd68b
fatimazyani <123373140+fatimazyani@users.noreply.github.com>
2023-03-07 23:41:09 +0100

    TASK 8

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,22 +1,10 @@
 #include "main.h"
 /**
- * *_strcat - concatenates two strings
- * @dest: pointer destination
- * @src: pointer source
- * Return: void
-*/
-
-char *_strcat(char *dest, char *src)
+ * set_string - sets pointer val to char
+ * @s: pointer
+ * @to: char
+ */
+void set_string(char **s, char *to)
 {
-int a = -1, i;
-for (i = 0; dest[i] != '\0'; i++)
-;
-
-do {
-	a++;
-	dest[i] = src[a];
-	i++;
-} while (src[a] != '\0');
-
-return (dest);
+	*s = to;
 }
