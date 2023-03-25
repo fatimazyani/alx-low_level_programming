721c33c435207c2ece72ac7e2e1b46870be31966
fatimazyani <123373140+fatimazyani@users.noreply.github.com>
2023-03-25 13:48:03 +0100

    TASK 100

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,23 +1,29 @@
 #include "main.h"
-
 /**
- * factorial - finds factorial
- * @n: int
- * Return: int
+ * _atoi - int
+ * @s: pointer
+ * Return: int.
  */
-
-int factorial(int n)
+int _atoi(char *s)
 {
+	int i;
+	int res = 0;
+	int sig = -1;
+	int brk = 0;
 
-	if (n < 0)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		return (-1);
+		if (s[i] == '-')
+			sig = sig * -1;
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			res = res * 10;
+			res -= (s[i] - '0');
+			brk = 1;
+		}
+		else if (brk == 1)
+			break;
 	}
-	else if (n == 0)
-	{
-		return (1);
-	}
-
-	return (n * factorial(n - 1));
-
+	res = sig * res;
+	return (res);
 }
