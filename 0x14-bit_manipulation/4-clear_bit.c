103241b4f1f4bcbb4590e2421a1a15e65a135643
fatimazyani <123373140+fatimazyani@users.noreply.github.com>
2023-05-05 02:48:31 +0200

    clear bit updated

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,5 +1,19 @@
 #include "main.h"
+/**
+ * set_b - at a given index will sets the value of a bit to 1 .
+ * @n: the Number to convert.
+ * @index:   the index, starting from 0 of the bit you want to get.
+ * Return: if correct will return the value of the bit at index index or -1 if an error occured.
+ */
+int set_b(unsigned long int *n, unsigned int index)
+{
+	unsigned long int a = 1;
 
+	if (index > 64)
+		return (-1);
+	*n = a << index | *n;
+	return (1);
+}
 /**
  * clear_bit - at a given index will sets the value of a bit to 0 .
  * @n: the Number to convert.
